43eb24f609e5f4a581c097acdee4fa0b8f677fa9
gorgbus <ondrejglet@gmail.com>
2024-08-10 02:04:24 +0200

    Added flash.nvim

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,5 +1,16 @@
 return {
 	"stevearc/oil.nvim",
 	opts = {},
+	config = function()
+		require("oil").setup({
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					return name == ".." or name == ".git"
+				end,
+			},
+		})
+	end,
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 }
